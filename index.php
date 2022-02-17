40f493dc5c4732945c5a65ae33eb97d9b0f003cb
Dhawan Solanki <91565429+dhawansolanki@users.noreply.github.com>
2022-02-17 22:45:25 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,511 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KWZ2MJJ');</script>
+<!-- End Google Tag Manager -->
+
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="description" content="I am Dhawan Solanki A Freelancer Web Developer. Lives in Silvassa, India. I usually make Websites / Mob Apllications using HTML, CSS, PHP, JAVASACRIPT, MySQL, BOOTSTRAP, WORDPRESS. | Web Design | Web Development" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="Keywords" content="Dhawan,Solanki,DhawanSolanki,dhawansolanki,dhawan,slanki,DHAWAN,SOLANKI,DHAWANSOLANKI,DHAWAN SOLANKI, webdeveloper,Silvassa, website, Dadra Nagar Haveli,Freelancer,designer,webdesigner">
+    <meta name="robots" content="index, follow">
+    <meta name="title" content="Dhawan Solanki | Full Stack Web Developer | Web Designer In Silvassa,DNH | Web Development">
+    <title ng-if="!titleName" class="ng-scope">Dhawan Solanki | Full Stack Web Developer | Web Designer In Silvassa,DNH | Web Development</title>
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+
+</head>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe class="gtm_iframe" src="https://www.googletagmanager.com/ns.html?id=GTM-KWZ2MJJ"
+height="0" width="0" ></iframe></noscript>
+<!-- End Google Tag     Manager (noscript) -->
+
+
+<!-- custom cursors  -->
+<div class="cursor-1"></div>
+<div class="cursor-2"></div>
+
+
+<div id="menu-bars" class="fas fa-bars"></div>
+    
+<!-- header section starts  -->
+
+<header>
+
+    <a href="#" class="logo"> <span>Dhawan</span> Solanki </a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#service">service</a>
+        <a href="#experience">experience</a>
+        <a href="#portfolio">portfolio</a>
+        <a href="#contact">contact</a>
+    </nav>
+
+    <div class="follow">
+        <a href="https://www.facebook.com/dhawansolanki2" class="fab fa-facebook-f" target="_blank"></a>
+        <a href="https://twitter.com/_dhawansolanki" class="fab fa-twitter" target="_blank"></a>
+        <a href="https://instagram.com/dhawansolanki_" class="fab fa-instagram" target="_blank"></a>
+        <a href="https://www.linkedin.com/in/dhawan-solanki-9588bb21b/" class="fab fa-linkedin" target="_blank"></a>
+        <a href="https://www.youtube.com/channel/UC6dVqbtKePkEke7rbiJVDZA" class="fab fa-youtube" target="_blank"></a>
+    </div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <span class="hi"> hi there... </span>
+        <h1> i am <span> Dhawan Solanki </span> </h1>
+        <p class="info"> i am a Full Stack Web Developer </p>
+        <p class="text"> I usually make Websites / Web Apllications using HTML5, CSS3, JAVASACRIPT, PHP, MySQL, BOOTSTRAP, WORDPRESS. </p>
+        <a href="#about" class="btn">More about me</a>
+    </div>
+
+    <div class="image">
+        <img src="images/banner_image2.png" alt="Dhawan Solanki A Freelance Web Developer from Silvassa India.">
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+<h2 class="heading"> about <span> me </span> </h2>
+
+<div class="row-1">
+
+    <div class="image">
+        <img src="images/home-img2.png" alt="Dhawan Solanki A Freelance Web Developer from Silvassa India.">
+    </div>
+
+    <div class="content">
+        <h3> my name is Dhawan Solanki & i am a Full Stack Web developer </h3>
+        <p>I have a serious passion for UI effects, animations and creating intuitive, dynamic user experiences. Well-organised person, problem solver, independent Freelancer with high attention to detail. An Introvert With Extra Ordinary Extrovert things! Interested in the entire frontend spectrum and working on ambitious projects with positive people.</p>
+        <p>I am a UI / UX designer. I have extensive experience in web designing; UI/UX & development with great ability of revolve ideas into design which communicates in a creative way. Each design created inspires & helps to achieve my goals as well as customer needs.</p>
+
+        <p>My work process involves right from visualizing how customers business can be reflected through the designs to landing the business perfectly with great execution and support.</p>
+
+        <p>I have mastered myself to design pixel perfect artworks which are off course loved and appreciated by customers. These extensive principles of my design have always helped me to build extensive client network throughout the globe. Enhancing knowledge and building designing capabilities through day-to-day project experiences.</p>
+        <div class="box-container">
+            <div class="box">
+                <p> <span> age : </span> 18 </p>
+                <p> <span> gender : </span> male </p>
+                <p> <span> language : </span>english, gujarati, hindi </p>
+                <p> <span> work : </span> Full Stack Web development     </p>
+                <p> <span> Hobby : </span> Guitar</p>
+            </div>
+            <div class="box">
+                <p> <span> freelance : </span> available </p>
+                <p> <span> phone : </span> &nbsp &nbsp &nbsp - </p>
+                <p> <span> email : </span>dhawansolanki2103@gmail.com</p>
+                <p> <span> country : </span> india </p>
+            </div>
+        </div>
+        <a href="#" class="btn">download CV</a>
+        <a href="#" class="btn">hire me</a>
+    </div>
+
+</div>
+
+<h2 class="heading"> <span> my </span> skills </h2>
+
+<div class="row-2">
+
+    <div class="skills">
+        <div class="progress">
+            <h3> web design <span> 95% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+        <div class="progress">
+            <h3> web development <span> 90% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+        <div class="progress">
+            <h3> UI design <span> 75% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+        <div class="progress">
+            <h3> seo marketing <span> 85% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+    </div>
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>HTML5</h3>
+            
+        </div>
+        <div class="box">
+            <h3>CSS3</h3>
+            
+        </div>
+        <div class="box">
+            <h3>JS</h3>
+            
+        </div>
+        <div class="box">
+            <h3>PHP</h3>
+            
+        </div>
+        <div class="box">
+            <h3>MySQL</h3>
+            
+        </div>
+
+    </div>
+
+</div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- service section starts  -->
+
+<section class="service" id="service">
+
+<h2 class="heading"> <span> my </span> services </h2>
+
+    <div class="box-container">
+
+        <div class="box">
+            <i class="fas fa-code"></i>
+            <h3>Web Development</h3>
+            <p>If the idea of selling online attracts you, we are here to transform it from a dream into a digital reality.</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-code"></i>
+            <h3>Web Design & Re-design</h3>
+            <p>We can turn it into dynamic and attractive to web visitors as well as search engines.</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-paint-brush"></i>
+            <h3>graphic design</h3>
+            <p>graphic design is the art or skill of combining text and pictures in advertisements, magazines, or books.</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-paint-brush"></i>
+            <h3>Web App Development</h3>
+            <p>An interactive web application readily amplifies user engagement. Our user-centred web apps are feature-rich, intuitive, and perform exceedingly well. </p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-mobile"></i>
+            <h3>responsive design</h3>
+            <p>Customer-focused, creating mobile-friendly responsively designed experiences that deliver tangible business results, Appnovation helps brands navigate any and all facets of the digital landscape.</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-bullhorn"></i>
+            <h3>seo marketing</h3>
+            <p>It's much easier to double your business by doubling your conversion rate than by doubling your traffic.</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-chart-line"></i>
+            <h3>digital marketing</h3>
+            <p>A full range of marketing solutions to help grow your business and increase sales.</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-envelope"></i>
+            <h3>email marketing</h3>
+            <p>Focus on growing your list all of the time as newer subscribers are more engaged, adding to healthier open rates and ROI.</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-envelope"></i>
+            <h3>Domain + Hosting</h3>
+            <p>Web Hosting Server is the lifeline of every website. It should be up and running 24/7.</p>
+            <p>Your Hosting, Our Responsibility.</p>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- experience section starts  -->
+
+<section class="experience" id="experience">
+
+    <h2 class="heading"> <span> my </span> Timeline </h2>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="content">
+                <span> 2 Oct 2003 </span>
+                <h3>Birthday</h3>
+ <p>today_equals="2nd October";</p>
+<p>if(today_equals=="2nd October") {</p>
+<p>print(“Happy Birthday to Me..!”);
+}</p>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="content">
+                <span>May 2019 </span>
+                <h3>Secondary Schooling</h3>
+                <p>Completed Class 10 From BAPS Swaminarayan Vidyamandir, Silvassa.</p>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="content">
+                <span>July 2021 </span>
+                <h3>Higher Secondary Schooling</h3>
+                <p>Completed Class 12 With SCIENCES From BAPS Swaminarayan Vidyamandir, Silvassa. </p>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="content">
+                <span> 2021 - 2025 </span>
+                <h3>Computer Science & Engineering</h3>
+                <p>1st Year In CSE from Bangalore Technological Institute.</p>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="content">
+                <span>December 2021</span>
+                <h3>Full Stack Web Developer Bootcamp</h3>
+                <p>Completed Python & Django Full Stack Web Developer Bootcamp on Udemy.</p>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="content">
+                <span>2021 - Present</span>
+                <h3>Freelancing</h3>
+                <p> I enjoy creating things that live on the internet.</p>
+                <p>I’m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you.</p>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- experience section ends -->
+
+<!-- portfolio section starts  -->
+
+<section class="portfolio" id="portfolio">
+
+    <h2 class="heading"> <span> my </span> portfolio </h2>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/thedost.png" alt="Dhawan Solanki A Freelance Web Developer from Silvassa India." >
+            <h3>The Dost</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-link"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="https://thedost.rf.gd" class="fas fa-search" target="_blank"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/nm.png" alt="Dhawan Solanki A Freelance Web Developer from Silvassa India.">
+            <h3>NM Sargam World</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-link"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="https://nmsargamworld.rf.gd" class="fas fa-search" target="_blank"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/img-3.jpg" alt="Dhawan Solanki A Freelance Web Developer from Silvassa India.">
+            <h3>Artifi</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-link"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="https://artifi.rf.gd" class="fas fa-search" target="_blank"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/img-4.jpg" alt="Dhawan Solanki A Freelance Web Developer from Silvassa India.">
+            <h3>The1 Feels</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-link"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="https://the1feels.rf.gd" class="fas fa-search" target="_blank"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/img-5.jpg" alt="Dhawan Solanki A Freelance Web Developer from Silvassa India.">
+            <h3>Sumago Infotech</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-link"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-search"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/img-6.jpg" alt="Dhawan Solanki A Freelance Web Developer from Silvassa India.">
+            <h3>Dhawan Solanki</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-link"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="https://dhawansolanki.cf" class="fas fa-search" target="_blank"></a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- portfolio section ends -->
+
+
+<?php 
+// $server = "sql310.epizy.com";
+// $user = "epiz_30211890";
+// $pass = "KbVA1EEhEZte153";
+// $database = "epiz_30211890_dhawansolanki";
+$server = "localhost";
+$user = "root";
+$pass = "dhawan";
+$database = "dhawansolanki";
+
+$conn = mysqli_connect($server, $user, $pass, $database);
+
+if (!$conn) {
+    die("<script>alert('Connection Failed.')</script>");
+}
+
+?>
+
+<?php 
+
+if (isset($_POST['submit'])) {
+  $name = isset($_POST['name']);
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $message = $_POST['message'];
+  
+  $sql = "INSERT INTO contact (name, email, phone, message)
+          VALUES ('$name', '$email','$phone','$message')";
+      $result = mysqli_query($conn, $sql);
+      if ($result) {
+        echo "<script>alert('Your Query Will Be Reviewed Soon!')</script>";
+        
+        $name = "";
+        $email = "";
+        $phone = "";
+        $message = "";
+        
+      } else {
+        echo "<script>alert('Woops! Something Wrong Went.')</script>";
+      }
+    } 
+
+?>
+
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+    <h2 class="heading"> contact <span> me </span> </h2>
+
+    <div class="icons-container">
+
+        <div class="icons">
+            <i class="fas fa-envelope"></i>
+            <h3>email</h3>
+            <p>dhawansolanki2103@gmail.com</p>
+            
+        </div>
+
+        <div class="icons">
+            <i class="fas fa-phone"></i>
+            <h3>number</h3>
+            <p>Not Available</p>
+            
+        </div>
+
+        <div class="icons">
+            <i class="fas fa-map-marker-alt"></i>
+            <h3>address</h3>
+            <p>Silvassa, Dadra Nagar Haveli, India.</p>
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <form action="" method="POST">
+
+            <input type="text" name="name *" placeholder="name" class="box" required>
+            <input type="email" name="email" placeholder="email" class="box" required>
+            <input type="number"  name="phone" placeholder="number" class="box" required>
+
+            <textarea name="message" placeholder="message" id="" cols="30" rows="10" required></textarea>
+            <button type="submit" name="submit" class="btn">Send Message</button>
+
+
+        </form>
+
+        <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59885.00238052136!2d72.97022501102124!3d20.266603341264844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be0cb5edb1ce063%3A0x65fe78dc1cdaeb1f!2sSilvassa%2C%20Dadra%20and%20Nagar%20Haveli%20and%20Daman%20and%20Diu!5e0!3m2!1sen!2sin!4v1638959872736!5m2!1sen!2sin" allowfullscreen="" loading="lazy"></iframe>
+
+    </div>
+    
+
+</section>
+
+<!-- contact section ends -->
+
+<!-- footer section  -->
+<footer class="footer"> &copy 2021 | Made with ❤️  <span> by Dhawan Solanki. </span> | all rights reserved.  </footer>
+
+
+
+
+    
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
